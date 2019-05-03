e0b9527ef87ba79df95b726726cf8efcf48e78f1
Junior Dugue <jrdugue@gmail.com>
2019-05-02 23:32:51 -0400

    learned to use timeline animation

--- js/gsAnimation.js
+++ js/gsAnimation.js
@@ -14,6 +14,7 @@ TweenMax.to(squareTo, 1, {
   backgroundColor: "red",
   borderRadius: "50%",
   repeat: 2, //wil repeat twice
+  yoyo: true //will create smoother effect, property is only avaiable in tweenmax, not tweenlite
 })
 
 
@@ -25,4 +26,20 @@ TweenMax.from(squareFrom, 1, {
   backgroundColor: "green",
   borderRadius: "50%",
   repeat: -1, //-1 will repeat forever
+  yoyo: true //will create smoother effect, property is only avaiable in tweenmax, not tweenlite
+})
+
+
+let squareTimeline = document.querySelector("#squareTimeline");
+
+let timeline = new TimelineMax();
+
+timeline.to(squareTimeline, 1, {
+  left: "50%"
+})
+timeline.to(squareTimeline, 0.5, {
+  borderRadius: "50%"
+})
+timeline.to(squareTimeline, 0.3, {
+  backgroundColor: "green"
 })
