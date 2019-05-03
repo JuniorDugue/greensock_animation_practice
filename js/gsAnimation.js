3167cd3cc4f89891f83b722e64fc01b61bca715e
Junior Dugue <jrdugue@gmail.com>
2019-05-02 23:17:06 -0400

    learned TweenMax to and from in GS, created squares in css to play around with simple animations

---
+++ js/gsAnimation.js
@@ -0,0 +1,21 @@
+//tweenlite
+//has core features of greensock, lightweight and can be expanded with plugins
+
+//tweenmax
+//has all of greensocks bells and whistles, has repeat, yoyo, and repeateDelay and comes with many plugins
+
+//first argument the function accepts is a DOM element, then a duration, then a property for us to animate
+
+//square will move TO where I specify it
+let squareTo = document.getElementById("squareTo");
+
+TweenMax.to(squareTo, 1, {
+  left: "50%"
+})
+
+//square will move FROM where I specify it
+let squareFrom = document.getElementById("squareFrom");
+
+TweenMax.from(squareFrom, 1, {
+  left: "50%"
+})
